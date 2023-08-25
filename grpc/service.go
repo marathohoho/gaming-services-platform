b856c64ac70711ad84281147f50b9a43614c0faa
Marat Amanzholov <codmarat@gmail.com>
2023-08-25 15:18:02 +0600

    Handle non-existent user wallet balance in gRPC calls

--- grpc/service.go
+++ grpc/service.go
@@ -2,6 +2,7 @@ package grpc
 
 import (
 	"context"
+	"errors"
 	"gaming-services-platform/internal/grpc"
 	"gaming-services-platform/internal/repositories"
 	"gaming-services-platform/proto"
@@ -30,6 +31,9 @@ func (gs *GrpcService) RegisterGrpcServer(server *grpcLib.Server) {
 func (gs *GrpcService) Get(ctx context.Context, req *proto.RequestBalance) (*proto.ResponseBalance, error) {
 	userWallet, err := gs.repository.Get(req.UserId)
 	if err != nil {
+		if err == redis.Nil {
+			return nil, errors.New("unable to find a wallet by userId provided")
+		}
 		return nil, err
 	}
 
